40199a8e005a1501bdbe51197e60afda0f94363e
Shres-Sumit <sumitcodeshrestha123@gmail.com>
2024-12-01 21:03:46 +0545

    login token generated and stored in localStorage also build Auth jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './Context/Auth.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <AuthProvider>
 
-    <StrictMode>
+    <BrowserRouter>
       <App />
-    </StrictMode>,
+    </BrowserRouter>
 
-  </BrowserRouter>
+  </AuthProvider>
 )
